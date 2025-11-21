a65114fed3f359333e1c439f5727267ccfc12419
AlexDaunorasNielsen <adni25@student.aau.dk>
2025-11-21 10:10:08 +0100

    Create main.c
    Lavede main

---
+++ main.c
@@ -0,0 +1,25 @@
+struct edge;
+struct node;
+typedef struct edge edge;
+typedef struct node node;
+
+struct node {
+    const int x;
+    const int y;
+    int neighbour_count;
+    edge** successors; //array af pointere
+    edge** predecessors; //array af pointere
+    int* g;
+    int* h;
+    int* rhs;
+};
+
+struct edge {
+    node* source;
+    double cost;
+    node* dest;
+};
+
+int main(void) {
+
+}
