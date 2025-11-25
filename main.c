b8249dac346ee271a608856169c315b9516518d4
AlexDaunorasNielsen <adni25@student.aau.dk>
2025-11-25 09:26:07 +0100

    Update main.c
    Importerede warehouse.h til main

--- main.c
+++ main.c
@@ -1,31 +1,5 @@
-struct edge;
-struct node;
-typedef struct edge edge;
-typedef struct node node;
-
-struct node {
-    const int x;
-    const int y;
-    int neighbour_count;
-    edge** successors; //array af pointere
-    edge** predecessors; //array af pointere
-    int* g;
-    int* h;
-    int* rhs;
-};
-
-struct edge {
-    node* source;
-    double cost;
-    node* dest;
-};
-
-int node_pos(int size_x, int, x int y);
+#include "warehouse.h"
 
 int main(void) {
 
-}
-
-int node_pos(int size_x, int, x int y){
-    return y * size_x + x;
 }
