fc45e3b1467342f2d44363c3721e9eb2c509c056
Mateusz Jagiello <mjagie25@student.aau.dk>
2025-12-01 19:10:40 +0100

    coded are 99% færdig der skal bare rodes lidt op + muligt optimaztion for randomization + sikre at der er sammenarbejde mellem lagergeneratoren

--- main.c
+++ main.c
@@ -1,98 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdint.h>
 
 #include "warehouse.h"
 
-void OrderRandomizer(int OrderAmount, order* OrderArray[], node* Pickup[], node* Dropoff[], node* Shelves[]);
+void OrderRandomizer(int OrderAmount, order* OrderArray[], node* Pickup[], int PickLength, node* Dropoff[], int DropLength, node* Shelves[], int ShelveLength);
 
 int main(void) {
-int OrderAmount;
-printf("amount of orders");
-scanf(" %d" , &OrderAmount);
-order* orderarray[OrderAmount];
+	int ShelveLength = 3, DropLength = 3,PickLength = 3;
 
-node* Shelves[99]; //MIDLERTIDIG VÆRDI FOR SIZE AF ARRAYET
-node* Pickup[99]; //MIDLERTIDIG VÆRDI FOR SIZE AF ARRAYET
-node* Dropoff[99]; //MIDLERTIDIG VÆRDI FOR SIZE AF ARRAYET
+	node* Shelves[ShelveLength];
+	Shelves[0] = &(node){10, 20};
+	Shelves[1] = &(node){30, 40};
+	Shelves[2] = &(node){50, 60};
+	//MIDLERTIDIG OG TILFÆLDIG VÆRDIER FOR ARRAYET
 
+	node* Pickup[DropLength]; //MIDLERTIDIG OG TILFÆLDIG VÆRDIER FOR ARRAYET
+	Pickup[0] = &(node){1, 2};
+	Pickup[1] = &(node){3, 4};
+	Pickup[2] = &(node){5, 6};
 
+	node* Dropoff[PickLength]; //MIDLERTIDIG OG TILFÆLDIG VÆRDIER FOR ARRAYET
+	Dropoff[0] = &(node){100, 200};
+	Dropoff[1] = &(node){300, 400};
+	Dropoff[2] = &(node){500, 600};
 
+	// VÆRDIERNE OVENOVER FORVENTES FRA LAGER GENERATOR
 
-}
-//find ud af om ARRAY size tingene tager hele længden eller kun der hvor der er variabler, fordi så skal de måske laves om
+	int OrderAmount;
+	printf("amount of orders");
+	scanf(" %d" , &OrderAmount);
+	order* OrderArray[OrderAmount];
+	OrderRandomizer(OrderAmount, OrderArray, Pickup, PickLength,Dropoff, DropLength, Shelves, ShelveLength);
 
-void OrderRandomizer(int OrderAmount, order* orderarray[], node* Pickup[], node* Dropoff[], node* Shelves[]){
- // anden ide til hvordan man kan lave random hylder orders lidt mere optimalt tror jeg
-	srand(time(NULL));
-
-for(int i = 0; i < OrderAmount; i++) //måske <= OrderAmount? skal lige teste først
-{
-	int pickordrop = random(1,2); //1 = pick up 2 = drop off. mening er de skal kunne ændres hver loop
-
-	if(pickordrop == 1){ //tilfæld pickup
-
-int indexofpickup = random(0, PickupArraySize);
-
-
-	orderarray[i].node_1 = Pickup[indexofpickup]; //sætter node_1 lige med et tilfældigt node fra pickuparrayet. pickuparrayet burde have både x og y værdierne for alle pickup noder.
-
-	int indexofshelve = random(0,hylderarraysize); //find et tilfældigt tal mellem 0 og den største indeks af hylder array
-
-
-	//coden over antager at hylde arrayet består kun af node structs som hver peger på et enkelt shelve, hvor i dens helhed har den koordinatner for alle shelves.
-	orderarray[i].node_2 = Shelves[indexofshelve];
-	i++;
-}
-
-	if(pickordrop == 2){ //tilfæld dropoff
-
-		int indexofshelve = random(0,hylderarraysize); //find et tilfældigt tal mellem 0 og den største indeks af hylder array
-
-		orderarray[i]->node_1 = Shelves[indexofshelve];
-		//coden over antager at hylde arrayet består kun af node structs som hver peger på et enkelt shelve, hvor i dens helhed har den koordinatner for alle shelves.
-
-
-		//TESTING GROUND FOR RANDOM
-		int DropoffLength = sizeof(Dropoff) / sizeof(Dropoff[0]);
-
-		int indexofdropoff = rand() % (DropoffLength - 0 +1)+0; //finder random værdi mellem maximum Dropofflength og minimum 0
-		// MAKE IT MORE RANDOM WITH PROBALY SRAND
-		orderarray[i]->node_2 = Dropoff[indexofdropoff];
-
-		i++;
+	// slet for loop i endeligt version nedenunder kun for test.
+	for (int i = 0; i < OrderAmount; i++) {
+		printf("Order %d: node_1 = (%d,%d), node_2 = (%d,%d)\n",
+				i+1,
+			   OrderArray[i]->node_1->x,
+			   OrderArray[i]->node_1->y,
+			   OrderArray[i]->node_2->x,
+			   OrderArray[i]->node_2->y);
 	}
 }
 
-}
+void OrderRandomizer(int OrderAmount, order* OrderArray[], node* Pickup[], int PickLength, node* Dropoff[], int DropLength, node* Shelves[], int ShelveLength){
 
+	unsigned int seed = (unsigned int)(time(NULL) ^ (uintptr_t)&seed); //mostly unpredictable seed for randomization
+	srand(seed); //inputs the seed into the random function
 
-
-
-
-
-
-/*
-if(pickordrop == 2) { //første ide til hvordan man kan lave random hylder orders
-
-for(int i = 0; i < orderamount; i++)
+for(int i = 0; i < OrderAmount; i++)
 {
-test.node_1.x = random(0,maxsizex);
-test.node_1.y = random(0,maxsizey);
+	OrderArray[i] = malloc(sizeof(order)); //allocation of space for the array.
+	if (OrderArray[i] == NULL) {
+		printf("Memory allocation failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-for(int j; j <hylderarraysize; j++)	{
-if(node_1 == hylderarray[j]){
-//gem node_1 værdien indeni et array of order MANGLER
-i++;
-							}
-else
-j++;
+	int rnd = rand();
+	int pickordrop = (rnd < RAND_MAX/2) ? 1 : 2; //1 = pick up 2 = drop off. mening er de skal kunne ændres hver loop
+  //rand chooses a number between 0 and RAND_MAX as such this generates a 50/50 chance for either pick or drop
 
+if(pickordrop == 1){ //tilfæld pickup
+	int indexofpickup = rand() % PickLength; //åbenbart er der et bias når man laver modulo af rand(), hvor mindre number dukker op mere ofte(måske find fix?)
+	OrderArray[i]->node_1 = Pickup[indexofpickup]; //sætter node_1 lige med et tilfældigt node fra pickuparrayet. pickuparrayet burde have både x og y værdierne for alle pickup noder.
 
-									}
+	int indexofshelve = rand() % ShelveLength; //find et tilfældigt tal mellem 0 og den største indeks af hylder array
+	//coden over antager at hylde arrayet består kun af node structs som hver peger på et enkelt shelve, hvor i dens helhed har den koordinatner for alle shelves.
+	OrderArray[i]->node_2 = Shelves[indexofshelve];
 }
 
-					}
+if(pickordrop == 2){ //tilfæld dropoff
+	int indexofshelve = rand() % ShelveLength; //det samme som shelve assigneren i tilfæld pickup
+	OrderArray[i]->node_1 = Shelves[indexofshelve];
+
+	int indexofdropoff = rand() % DropLength; //det samme som de andre assigner, bare at den assigner fra Dropoff arrayet
+	OrderArray[i]->node_2 = Dropoff[indexofdropoff];
+}
 
 }
-*/
+
+}
