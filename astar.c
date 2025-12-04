d50b186270ca8e7719bf70d4544ef6b0be61b537
victordueandersen <vdan25@student.aau.dk>
2025-12-04 11:03:57 +0100

    Fixed some comments

--- astar.c
+++ astar.c
@@ -1,91 +1,73 @@
 #include <math.h>
 #include "warehouse.h"
 
-int OPENNODESAMOUNT = 100;
-int opennodes[OPENNODESAMOUNT][2];
-
 //placeholder
 int optimalpath[OPENNODESAMOUNT][2];
 
 
-void astar(node** nodes, int startx, int starty, int goalx, int goaly, int x_size);
-void calcsucceding(node** nodes, int x, int y, int localgh, int goalx, int goaly);
+void astar(node** nodes, int start_x, int start_y, int goal_x, int goal_y, int map_id, int* open_nodes, int* optimal_path);
+void calcsucceding(node** nodes, int x, int y, int map_id, int goalx, int goaly);
 int hcalc(int x, int y, int goalx, int goaly);
 void addToOpennodes(int x, int y);
-void calcastar(node** nodes, int startx, int starty, int goalx, int goaly, int localgh);
+void calcastar(node** nodes, int startx, int starty, int goalx, int goaly, int map_id);
 
-void astar(node** nodes, int startx, int starty, int goalx, int goaly, int x_size) {
-    //placeholder
-    for (int i = 0; i < OPENNODESAMOUNT; i++) {
-        optimalpath[i][0] = INFINITY;
-        optimalpath[i][1] = INFINITY;
-    }
-
-    for (int i = 0; i < OPENNODESAMOUNT; i++) {
-        opennodes[i][0] = INFINITY;
-        opennodes[i][1] = INFINITY;
-    }
-
-    int localgh = node_pos(x_size, startx, starty);
+void astar(node** nodes, int startx, int starty, int goalx, int goaly, int map_id, int* open_nodes, int* optimal_path) {
 
     //Calculating starting node
-    nodes[starty][startx].g[localgh] = 0;
-    nodes[starty][startx].h[localgh] = hcalc(startx, starty, goalx, goaly);
+    nodes[starty][startx].g[map_id] = 0;
+    nodes[starty][startx].h[map_id] = hcalc(startx, starty, goalx, goaly);
 
-    int nx = startx,
-        ny = starty;
+    int temp_x = startx,
+        temp_y = starty;
 
     //Check all nodes with lowest f(n) until the open node with lowest f(n) is the goal node
-    while (nodes[goaly][goalx].g[localgh] == INFINITY) {
-        calcsucceding(nodes, nx, ny, localgh, goalx, goaly);
+    while (nodes[goaly][goalx].g[map_id] == INFINITY) {
+        calcsucceding(nodes, temp_x, temp_y, map_id, goalx, goaly);
 
-        int n = 0,
+        int temp = 0,
             i = 0,
             lowestf = INFINITY;
 
         //check open nodes for lowest f(n)
-        while (n == 0) {
-            //If y value is infinity we have reached the end of open nodes
-            if (opennodes[i][0] == INFINITY) {
-                n = 1;
-            } else if (opennodes[i][0] >= 0) {
+        while (opennodes[i][0] != INFINITY) {
+            int current_f = (nodes[opennodes[i][0]][opennodes[i][1]].g[map_id]+
+                             nodes[opennodes[i][0]][opennodes[i][1]].h[map_id]);
+
+            if (opennodes[i][0] >= 0) {
                 //Check if y is 0 or above as closed nodes get value of -1 to replace them.
-                if ((nodes[opennodes[i][0]][opennodes[i][1]].g[localgh]+
-                     nodes[opennodes[i][0]][opennodes[i][1]].h[localgh]) < lowestf) {
-                    ny = opennodes[i][0];
-                    nx = opennodes[i][1];
-                    lowestf = (nodes[opennodes[i][0]][opennodes[i][1]].g[localgh]+
-                               nodes[opennodes[i][0]][opennodes[i][1]].h[localgh]);
+                if (current_f < lowestf) {
+                    temp_y = opennodes[i][0];
+                    temp_x = opennodes[i][1];
+                    lowestf = current_f;
                 }
             }
             i++;
         }
     }
 
-    calcastar(nodes, startx, starty, goalx, goaly, localgh);
+    calcastar(nodes, startx, starty, goalx, goaly, map_id);
 
 }
 
-void calcsucceding(node** nodes, int x, int y, int localgh, int goalx, int goaly) {
-    //Helper int
-    int n = 0;
+void calcsucceding(node** nodes, int x, int y, int map_id, int goalx, int goaly) {
+    //Temporary int
+    int temp = 0;
 
-    for (int i = 0; i < 8; i++) {
-        //Setting helper int to the calculated new g for the succeding node
-        n = (nodes[y][x].successors[i].cost + nodes[y][x].g[localgh]);
+    for (int i = 0; i < nodes[starty][startx].neighbor_count; i++) {
+        //Setting temporary int to the calculated new g for the succeding node
+        temp = (nodes[y][x].successors[i].cost + nodes[y][x].g[map_id]);
 
         //Checking if node is already calculated once
-        if (nodes[y][x].successors[i].dest.g[localgh] != INFINITY) {
-            //If node is already calcualed check if new calculated g is lower than old
-            if (nodes[y][x].successors[i].dest.g[localgh] > n) {
-                nodes[y][x].successors[i].dest.g[localgh] = n;
-                nodes[y][x].successors[i].dest.h[localgh] =
+        if (nodes[y][x].successors[i].dest.g[map_id] != INFINITY && nodes[y][x].successors[i].dest.g[map_id] > temp) {
+
+                nodes[y][x].successors[i].dest.g[map_id] = temp;
+                nodes[y][x].successors[i].dest.h[map_id] =
                 hcalc(nodes[y][x].successors[i].dest.x, nodes[y][x].successors[i].dest.y, goalx, goaly);
                 addToOpennodes(x,y);
-            }
+
         } else {
-            nodes[y][x].successors[i].dest.g[localgh] = n;
-            nodes[y][x].successors[i].dest.h[localgh] =
+            nodes[y][x].successors[i].dest.g[map_id] = temp;
+            nodes[y][x].successors[i].dest.h[map_id] =
             hcalc(nodes[y][x].successors[i].dest.x, nodes[y][x].successors[i].dest.y, goalx, goaly);
             addToOpennodes(x,y);
         }
@@ -95,25 +77,26 @@ void calcsucceding(node** nodes, int x, int y, int localgh, int goalx, int goaly
 }
 
 int hcalc(int x, int y, int goalx, int goaly) {
-    return sqrt(((goalx-x)*(goalx-x))+((goaly-y)*(goaly-y)));
-}
-
-void addToOpennodes(int x, int y) {
-    int n = 0,
-        i = 0;
-
-    while (n == 0) {
-
-        if (opennodes[i][0] != INFINITY) {
-            y = opennodes[i][0];
-            x = opennodes[i][1];
-            n++;
-        }
-        i++;
+    if ((goalx-x) > (goaly-y) {
+        return ((goalx-x-goaly+y)+sqrt((goaly-y)*(goaly-y)*2)
+    } else {
+        return ((goaly-y-goalx+x)+sqrt((goalx-x)*(goalx-x)*2)
     }
 }
 
-void calcastar(node** nodes, int startx, int starty, int goalx, int goaly, int localgh) {
+void addToOpennodes(int x, int y) {
+
+        i = 0;
+
+    while (opennodes[i][0] != INT_MAX || opennodes[i][0] != -1) {
+        i++;
+    }
+    opennodes[i][0] = y;
+    opennodes[i][1] = x;
+}
+
+/*
+void calcastar(node** nodes, int startx, int starty, int goalx, int goaly, int map_id) {
     //helping int
     int x = goalx,
         y = goaly,
@@ -125,17 +108,17 @@ void calcastar(node** nodes, int startx, int starty, int goalx, int goaly, int l
     numberstep = 1;
 
     while (x != startx && y != starty) {
-        //Helper int
-        int n = INFINITY;
+        //Temporary int
+        int temp= INFINITY;
 
-        int nx = x,
-            ny = y;
+        int temp_x = x,
+            temp_y = y;
 
         for (int i = 0; i < 8; i++) {
-            if (nodes[ny][nx].successors[i].dest.g[localgh] < n) {
-                n = nodes[ny][nx].successors[i].dest.g[localgh];
-                x = nodes[ny][nx].successors[i].dest.x;
-                y = nodes[ny][nx].successors[i].dest.y;
+            if (nodes[temp_y][temp_x].successors[i].dest.g[map_id] < n) {
+                temp = nodes[temp_y][temp_x].successors[i].dest.g[map_id];
+                x = nodes[temp_y][temp_x].successors[i].dest.x;
+                y = nodes[temp_y][temp_x].successors[i].dest.y;
             }
         }
 
@@ -145,3 +128,4 @@ void calcastar(node** nodes, int startx, int starty, int goalx, int goaly, int l
         numberstep++;
     }
 }
+*/
