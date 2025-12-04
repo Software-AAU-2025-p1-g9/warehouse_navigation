51f40bb6e78fec2bc4ca91a678ff2c4c283116e2
victordueandersen <vdan25@student.aau.dk>
2025-12-04 13:42:28 +0100

    Added resetg to headerfiler

--- astar.h
+++ astar.h
@@ -4,4 +4,5 @@
 #define ASTAR_H
 
 void astar(node** nodes, int start_x, int start_y, int goal_x, int goal_y, int map_id, int total_area, int **open_nodes);
+void resetg(node** nodes, int x_size, int y_size, int map_id);
 #endif //ASTAR_H.h"
