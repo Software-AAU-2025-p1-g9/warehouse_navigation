2921207a86beac2b87d053c6266f00828ce3af2d
victordueandersen <vdan25@student.aau.dk>
2025-12-04 13:18:58 +0100

    Added header file

---
+++ astar.h
@@ -0,0 +1,7 @@
+#include "warehouse.h"
+
+#ifndef ASTAR_H
+#define ASTAR_H
+
+void astar(node** nodes, int start_x, int start_y, int goal_x, int goal_y, int map_id, int total_area, int **open_nodes);
+#endif //ASTAR_H.h"
